73e159f682ef17735ce6fb61331436dd9427ed31
Suresh-sade <msgmanagementservice@gmail.com>
2024-08-15 18:03:41 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+@RestController
+@RequestMapping("/api")
+class ApiController {
+
+	@GetMapping("/greeting")
+	public String greeting() {
+		return "Hello, World!";
+	}
+}
